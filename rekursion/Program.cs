463a69c3f9097668a6c32a5e4fb585dba36e35a8
bluuo <115736650+bluuo@users.noreply.github.com>
2023-03-07 19:38:50 +0100

    added project rekursion

---
+++ rekursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace first_recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie eine ganze Zahl ein:");
+            int zahl = Convert.ToInt32(Console.ReadLine());
+            int fakultätErgebnis = faculty(zahl);
+            Console.WriteLine("Die Fakultät von:" + zahl + "ist" + fakultätErgebnis);
+        }
+
+        static public int faculty(int i)
+        {
+            if (i == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int r = faculty(i - 1);
+                return r;
+            };
+        }
+
+    }
+}
